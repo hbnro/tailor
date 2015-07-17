56a68a778f5310f9a6e194ae816b1d436b966a96
Alvaro Cabrera <pateketrueke@gmail.com>
2015-07-17 18:44:05 -0500

    Shortcut helper for

--- spec/all-spec.php
+++ spec/all-spec.php
@@ -2,9 +2,13 @@
 
 \Tailor\Base::initialize();
 
+function load($engine, $code) {
+  return \Tailor\Base::parse($engine, $code);
+}
+
 describe('Engines', function() {
   it('should parse Markdown', function() {
-    $view = \Tailor\Base::parse('markdown', '[Link](#)');
+    $view = load('markdown', '[Link](#)');
 
     expect($view)->toContain('<p><a href="#">Link</a></p>');
   });
